71639de8f16f2f95af4630635305f9086b8ef7e2
francescofeston <francescofeston@gmail.com>
2020-12-02 17:50:20 +0100

    modified player.java

--- src/guessint/Player.java
+++ src/guessint/Player.java
@@ -4,13 +4,33 @@ public class Player {
     private String guessedIntString;
     private int guessedInt;
 
-    public Player(){
+    public Player() {
+    }
+
+    public int playingGame() {
+        boolean qualified = false;
+        while (!qualified) {
+            guessedIntString = JOptionPane.showInputDialog("Enter a guess between 1 and 100");
+            qualified = isQualifiedData(guessedInt);
+            System.out.println("Player: my guess is " + guessedInt);
+            return guessedInt;
+        }
+        shwMsg("Player: my guess is" + guessedInt);
+        return guessedInt;
+    }
+    public boolean isQualifiedData(String inputString){
+        try{
+            guessedInt = Integer.parseInt(inputString);
+        } catch (NumberFormatException ex){
+            System.out.println();
+            isInteger(inputString);
+            return false;
+        }
+        return isInRange(); //check the range, returns false or true
 
     }
-    public int playingGame(){
-        guessedIntString = JOptionPane.showInputDialog("Enter a guess between 1 and 100");
-        guessedInt = Integer.parseInt(guessedIntString);
-        System.out.println("Player: my guess is " + guessedInt);
-        return guessedInt;
+
+    public boolean isInteger(String inputString){
+        
     }
 }
